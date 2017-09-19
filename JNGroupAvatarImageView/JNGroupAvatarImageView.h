32163c1c32d5d0b64ea5e12547acdbb54d07b795
mohammadnabulsi <mohammad.s.nabulsi@gmail.com>
2017-09-19 18:55:29 +0300

    Initial Commit

---
+++ JNGroupAvatarImageView/JNGroupAvatarImageView.h
@@ -0,0 +1,19 @@
+//
+//  JNGroupAvatarImageView.h
+//  JNGroupAvatarImageView
+//
+//  Created by Mohammad Nabulsi on 9/19/17.
+//  Copyright © 2017 JNDisrupter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JNGroupAvatarImageView.
+FOUNDATION_EXPORT double JNGroupAvatarImageViewVersionNumber;
+
+//! Project version string for JNGroupAvatarImageView.
+FOUNDATION_EXPORT const unsigned char JNGroupAvatarImageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JNGroupAvatarImageView/PublicHeader.h>
+
+
